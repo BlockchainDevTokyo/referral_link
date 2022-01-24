ddd4ea7157a48ea475708814f10940ff25097d0f
dinhnhat0401 <nhatphutho@gmail.com>
2022-01-25 00:15:04 +0900

    smart contract login/refer link

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,21 +1,76 @@
+import 'regenerator-runtime/runtime';
+import React, { useEffect, useState } from 'react';
+
 import { Meta } from '../layout/Meta';
 import { Main } from '../templates/Main';
+import {
+  initContract,
+  signIn,
+  signOut,
+  referLink,
+  getBalance,
+} from '../utils/NearAPI';
 
-const About = () => (
-  <Main meta={<Meta title="Lorem ipsum" description="Lorem ipsum" />}>
-    <p>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione fuga
-      recusandae quidem. Quaerat molestiae blanditiis doloremque possimus labore
-      voluptatibus distinctio recusandae autem esse explicabo molestias officia
-      placeat, accusamus aut saepe.
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione fuga
-      recusandae quidem. Quaerat molestiae blanditiis doloremque possimus labore
-      voluptatibus distinctio recusandae autem esse explicabo molestias officia
-      placeat, accusamus aut saepe.
-    </p>
-  </Main>
-);
+const About = () => {
+  const [contract, setContract] = useState<any>(null);
+  const [currentUser, setCurrentUser] = useState<any>(null);
+  const [nearConfig, setNearConfig] = useState<any>(null);
+  const [wallet, setWallet] = useState<any>(null);
+  const [balance, setBalance] = useState('');
+  const [disabled, setDisabled] = useState(false);
+  useEffect(() => {
+    async function fetchData() {
+      const data = await initContract();
+      setContract(data.contract);
+      setCurrentUser(data.currentUser);
+      setNearConfig(data.nearConfig);
+      setWallet(data.walletConnection);
+      const b = await getBalance(data.walletConnection);
+      setBalance(b);
+      setDisabled(false);
+    }
+    fetchData();
+  }, []);
+  return (
+    <Main meta={<Meta title="Lorem ipsum" description="Lorem ipsum" />}>
+      <p>
+        {currentUser ? (
+          <p>
+            Currently signed in as:{' '}
+            <code>
+              {currentUser.accountId} ({balance}){' '}
+            </code>
+          </p>
+        ) : (
+          <p>Please login to continue.</p>
+        )}
+      </p>
+      {currentUser ? (
+        <button onClick={() => signOut(wallet)}>Log out</button>
+      ) : (
+        <button onClick={() => signIn(wallet, nearConfig)}>Log in</button>
+      )}
+      <p>
+        {currentUser ? (
+          <button
+            onClick={async () => {
+              setDisabled(true);
+              await referLink(contract, currentUser, 'test.net', 1);
+              const b = await getBalance(wallet);
+              setBalance(b);
+
+              setDisabled(false);
+            }}
+            disabled={disabled}
+          >
+            Refer Link
+          </button>
+        ) : (
+          <p>Please login to continue.</p>
+        )}
+      </p>
+    </Main>
+  );
+};
 
 export default About;
---
+++ src/utils/NearAPI.ts
@@ -0,0 +1,104 @@
+import Big from 'big.js';
+import * as nearAPI from 'near-api-js';
+import { utils } from 'near-api-js';
+
+import { getConfig } from './NearConfig';
+
+const BOATLOAD_OF_GAS = Big(3)
+  .times(10 ** 13)
+  .toFixed();
+
+// Initializing contract
+export async function initContract() {
+  const nearConfig = getConfig(process.env.NODE_ENV);
+
+  // const keystore = new nearAPI.keyStores.InMemoryKeyStore();
+  // const keyPair = nearAPI.KeyPair.fromString(
+  //   process.env.NEXT_PUBLIC_SEND_PRIVATE_KEY || ''
+  // );
+  // keystore.setKey(nearConfig.networkId, nearConfig.contractName, keyPair);
+
+  // // Initializing connection to the NEAR TestNet
+  // const near = await nearAPI.connect({
+  //   keyStore: new nearAPI.keyStores.BrowserLocalStorageKeyStore(),
+  //   ...nearConfig,
+  //   headers: {},
+  // });
+
+  const keystore = new nearAPI.keyStores.InMemoryKeyStore();
+  const keyPair = nearAPI.KeyPair.fromString(
+    process.env.NEXT_PUBLIC_SEND_PRIVATE_KEY || ''
+  );
+  keystore.setKey(nearConfig.networkId, nearConfig.contractName, keyPair);
+  // Initializing connection to the NEAR TestNet
+  const near = await nearAPI.connect({
+    // keyStore: new nearAPI.keyStores.BrowserLocalStorageKeyStore(),
+    keyStore: keystore,
+    ...nearConfig,
+    headers: {},
+  });
+
+  // Needed to access wallet
+  const walletConnection = new nearAPI.WalletConnection(near, null);
+
+  const masterAccount = await new nearAPI.Account(
+    near.connection,
+    nearConfig.contractName
+  );
+
+  // Load in account data
+  let currentUser;
+  if (walletConnection.getAccountId()) {
+    currentUser = {
+      accountId: walletConnection.getAccountId(),
+      balance: (await walletConnection.account().state()).amount,
+    };
+    // currentUser = walletConnection.getAccountId();
+  }
+
+  // Initializing our contract APIs by contract name and configuration
+  const contract = new nearAPI.Contract(
+    masterAccount,
+    nearConfig.contractName,
+    {
+      // View methods are read-only – they don't modify the state, but usually return some value
+      viewMethods: ['get_link'],
+      // Change methods can modify the state, but you don't receive the returned value when called
+      changeMethods: ['click_link'],
+      // Sender is the account ID to initialize transactions.
+      // getAccountId() will return empty string if user is still unauthorized
+      // sender: walletConnection.getAccountId(),
+    }
+  );
+
+  return { contract, currentUser, nearConfig, walletConnection };
+}
+
+export function signOut(wallet: any) {
+  wallet.signOut();
+}
+
+export function signIn(wallet: any, nearConfig: any) {
+  wallet.requestSignIn(nearConfig.contractName, 'Coinmarketfi Dapp');
+}
+
+export async function referLink(
+  contract: any,
+  currentUser: any,
+  link: any,
+  amount: any
+) {
+  await contract.click_link(
+    {
+      url: link,
+      account_id: currentUser.accountId,
+    },
+    BOATLOAD_OF_GAS,
+    utils.format.parseNearAmount(amount.toString())
+  );
+}
+
+export async function getBalance(wallet: any) {
+  const balance = (await wallet.account().state()).amount;
+  return utils.format.formatNearAmount(balance);
+}
