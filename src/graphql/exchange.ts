b2c80abd99a1844339cf06629a227c781964f72f
dinhnhat0401 <nhatphutho@gmail.com>
2022-01-29 10:25:03 +0900

    Added apollo

--- src/graphql/exchange.ts
+++ src/graphql/exchange.ts
@@ -1,10 +1,12 @@
 import { gql } from '@apollo/client';
 
 const EXCHANGES = gql`
-  query exchanges {
-    exchanges {
-      name
+  query ($sortBy: ExchangeSortByInput) {
+    exchanges(sortBy: $sortBy) {
       id
+      name
+      trade_volume_24h_btc_normalized
+      centralized
       country
       image
       url
