3e35b1eada523cfc88a76ec2eaf9de451acadd68
dinhnhat0401 <nhatphutho@gmail.com>
2022-02-05 17:35:47 +0900

    Landing page

--- src/templates/Main.tsx
+++ src/templates/Main.tsx
@@ -1,5 +1,7 @@
 import { ReactNode } from 'react';
 
+import Script from 'next/script';
+
 import { AppConfig } from '../utils/AppConfig';
 import Nav from './nav';
 
@@ -13,13 +15,37 @@ const Main = (props: IMainProps) => (
     {props.meta}
 
     <div className="max-w-screen-xl mx-auto">
+      <div id="market" className="text-center"></div>
+      <Script
+        id="stripe-js"
+        src="https://js.stripe.com/v3/"
+        onLoad={() => {
+          const baseUrl = 'https://widgets.cryptocompare.com/';
+          const market = document.getElementById('market');
+          (() => {
+            let appName = encodeURIComponent(window.location.hostname);
+            if (appName === '') {
+              appName = 'local';
+            }
+
+            const s = document.createElement('script');
+            s.type = 'text/javascript';
+            s.async = true;
+            const theUrl = `${baseUrl}serve/v3/coin/header?fsyms=BTC,ETH,CELO&tsyms=USD`;
+            s.src = `${
+              theUrl + (theUrl.indexOf('?') >= 0 ? '&' : '?')
+            }app=${appName}`;
+            market?.appendChild(s);
+          })();
+        }}
+      />
       <div className="border-b border-gray-300">
-        <div className="pt-16 pb-8">
+        {/* <div className="pt-16 pb-8">
           <div className="font-bold text-3xl text-gray-900">
             {AppConfig.title}
           </div>
           <div className="text-xl">{AppConfig.description}</div>
-        </div>
+        </div> */}
         <div>{Nav()}</div>
       </div>
       <div className="py-5 text-xl content">{props.children}</div>
--- src/templates/nav.tsx
+++ src/templates/nav.tsx
@@ -1,57 +1,114 @@
+// import { i18 } from '../utils/I18';
+import { Fragment } from 'react';
+
+import { Popover, Transition } from '@headlessui/react';
+import { MenuIcon, XIcon } from '@heroicons/react/outline';
 import Link from 'next/link';
 
-// import { i18 } from '../utils/I18';
+const navigation = [
+  { name: 'Cryptocurrencies', href: '/cryptocurrencies' },
+  { name: 'Exchanges', href: '/exchanges' },
+  { name: 'Marketplace', href: '/where-to-buy' },
+  { name: 'About', href: '/about' },
+];
 
 export default function Nav() {
   return (
-    <ul className="flex flex-wrap text-xl">
-      <li className="mr-6">
-        <Link href="/">
-          <a className="text-gray-700 border-none hover:text-gray-900">
-            {/* {i18('home', 'Home')} */}
-            {'Home'}
-          </a>
-        </Link>
-      </li>
-      <li className="mr-6">
-        <a
-          className="text-gray-700 border-none hover:text-gray-900"
-          href="/cryptocurrencies/"
+    <Popover>
+      <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
+        <nav
+          className="relative flex items-center justify-between sm:h-10 lg:justify-start"
+          aria-label="Global"
         >
-          {'Cryptocurrencies'}
-          {/* {i18('cryptocurrencies', 'Cryptocurrencies')} */}
-        </a>
-      </li>
-      <li className="mr-6">
-        <div className="dropdown relative">
-          <Link href="/exchanges/">
-            <a className="text-gray-700 hover:text-gray-900">
-              {'Exchanges'}
-              {/* {i18('exchanges', 'Exchanges')} */}
-            </a>
-          </Link>
-          <div className="dropdown-content w-max bg-gray-100 rounded-md shadow-xl">
-            <Link href="/dex/">
-              <a className="block px-4 py-2 text-gray-700 hover:text-gray-900 hover:bg-gray-200 hover:border-none">
-                {'DEX'}
-                {/* {i18('dex', 'DEX')} */}
-              </a>
-            </Link>
-            <Link href="/cex/">
-              <a className="block px-4 py-2 text-gray-700 hover:text-gray-900 hover:bg-gray-200 hover:border-none">
-                {'CEX'}
-                {/* {i18('cex', 'CEX')} */}
-              </a>
-            </Link>
-            <Link href="/where-to-buy/">
-              <a className="block px-4 py-2 text-gray-700 hover:text-gray-900 hover:bg-gray-200 hover:border-none">
-                {'Where to buy'}
-                {/* {i18('where-to-buy', 'Where to buy')} */}
-              </a>
-            </Link>
+          <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
+            <div className="flex items-center justify-between w-full md:w-auto">
+              <Link href="/">
+                <a>
+                  <span className="sr-only">Workflow</span>
+                  <img
+                    alt="cmf-logo"
+                    className="h-8 w-auto sm:h-10"
+                    src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                  />
+                </a>
+              </Link>
+              <div className="-mr-2 flex items-center md:hidden">
+                <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                  <span className="sr-only">Open main menu</span>
+                  <MenuIcon className="h-6 w-6" aria-hidden="true" />
+                </Popover.Button>
+              </div>
+            </div>
           </div>
-        </div>
-      </li>
-    </ul>
+          <div className="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
+            {navigation.map((item) => (
+              <a
+                key={item.name}
+                href={item.href}
+                className="font-medium text-gray-500 hover:text-gray-900"
+              >
+                {item.name}
+              </a>
+            ))}
+            <a
+              href="#"
+              className="font-medium text-indigo-600 hover:text-indigo-500"
+            >
+              Log in
+            </a>
+          </div>
+        </nav>
+      </div>
+
+      <Transition
+        as={Fragment}
+        enter="duration-150 ease-out"
+        enterFrom="opacity-0 scale-95"
+        enterTo="opacity-100 scale-100"
+        leave="duration-100 ease-in"
+        leaveFrom="opacity-100 scale-100"
+        leaveTo="opacity-0 scale-95"
+      >
+        <Popover.Panel
+          focus
+          className="absolute z-10 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
+        >
+          <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
+            <div className="px-5 pt-4 flex items-center justify-between">
+              <div>
+                <img
+                  className="h-8 w-auto"
+                  src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                  alt=""
+                />
+              </div>
+              <div className="-mr-2">
+                <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                  <span className="sr-only">Close main menu</span>
+                  <XIcon className="h-6 w-6" aria-hidden="true" />
+                </Popover.Button>
+              </div>
+            </div>
+            <div className="px-2 pt-2 pb-3 space-y-1">
+              {navigation.map((item) => (
+                <a
+                  key={item.name}
+                  href={item.href}
+                  className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+                >
+                  {item.name}
+                </a>
+              ))}
+            </div>
+            <a
+              href="#"
+              className="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-50 hover:bg-gray-100"
+            >
+              Log in
+            </a>
+          </div>
+        </Popover.Panel>
+      </Transition>
+    </Popover>
   );
 }
